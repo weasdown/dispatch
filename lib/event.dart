4943aa2bf071b57884a1e99c3c905d13a88a582f
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-25 07:32:27 +0100

    feat: add placeholder address handling in Event

--- lib/event.dart
+++ lib/event.dart
@@ -2,8 +2,17 @@ import 'package:latlong2/latlong.dart';
 
 /// An emergency event that the ambulance service has become aware of.
 class Event {
-  Event({required this.location});
+  Event({required this.address}) : location = _locationFromAddress(address);
+
+  /// The street address of the emergency.
+  final String address;
 
   /// The latitude and longitude of the emergency.
   final LatLng location;
+
+  /// Returns the latitude and longitude of a given street [address].
+  static LatLng _locationFromAddress(String address) {
+    // TODO: implement _locationFromAddress
+    return LatLng(0, 0);
+  }
 }
