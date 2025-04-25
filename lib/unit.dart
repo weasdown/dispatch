789fd3145f6b11e4bc7bb21ed16a67cffa3fe8cb
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-25 07:26:20 +0100

    feat: add unit.dart with Unit class

---
+++ lib/unit.dart
@@ -0,0 +1,7 @@
+/// An ambulance vehicle, air ambulance or other resource.
+class Unit {
+  Unit({required this.callsign});
+
+  /// A unique identifier for this resource.
+  final String callsign;
+}
