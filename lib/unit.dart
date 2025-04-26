1ef5ab7a8867551d3a7aa46471536050cbddf0fe
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-26 22:58:13 +0100

    feat: add Unit.location, defaultUnits

--- lib/unit.dart
+++ lib/unit.dart
@@ -1,7 +1,31 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 /// An ambulance vehicle, air ambulance or other resource.
 class Unit {
-  Unit({required this.callsign});
+  Unit({required this.callsign, required this.location});
 
   /// A unique identifier for this resource.
   final String callsign;
+
+  /// The current latitude and longitude of this unit.
+  LatLng location;
 }
+
+final List<Unit> defaultUnits = [
+  Unit(
+    callsign: 'NA136',
+    location: LatLng(51.607539604000266, -1.237806756282358),
+  ),
+  Unit(
+    callsign: 'NA283',
+    location: LatLng(51.616072911907786, -1.2536017723108663),
+  ),
+  Unit(
+    callsign: 'NA072',
+    location: LatLng(51.8296012219854, -1.3134667111590494),
+  ),
+  Unit(
+    callsign: 'NR154',
+    location: LatLng(51.89096056450592, -1.1499680791975175),
+  ),
+];
