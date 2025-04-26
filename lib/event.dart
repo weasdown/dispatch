245758c62f7cd24f7671cc6ab72fa6af24a9e9a3
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-26 21:49:22 +0100

    feat: add EventListModel

--- lib/event.dart
+++ lib/event.dart
@@ -1,3 +1,6 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
 
 /// An emergency event that the ambulance service has become aware of.
@@ -14,5 +17,38 @@ class Event {
   static LatLng _locationFromAddress(String address) {
     // TODO: implement _locationFromAddress
     return LatLng(0, 0);
+  }
+}
+
+/// Storage for all the events that units will respond to.
+class EventListModel extends ChangeNotifier {
+  EventListModel({required List<Event> events}) {
+    _events.addAll(events);
+  }
+
+  EventListModel.blank();
+
+  /// Internal, private state of the event list.
+  final List<Event> _events = [];
+
+  /// An unmodifiable view of the events in the event list.
+  UnmodifiableListView<Event> get events => UnmodifiableListView(_events);
+
+  /// The current total number of events.
+  int get numEvents => _events.length;
+
+  /// Adds [event] to event list. This and [removeAll] are the only ways to
+  /// modify the event list from the outside.
+  void add(Event event) {
+    _events.add(event);
+    // This call tells the widgets that are listening to this model to rebuild.
+    notifyListeners();
+  }
+
+  /// Removes all events from the event list.
+  void removeAll() {
+    _events.clear();
+    // This call tells the widgets that are listening to this model to rebuild.
+    notifyListeners();
   }
 }
