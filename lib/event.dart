dd1c5048f65b898c1815d9ac4989fb09376b3f11
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-26 22:48:46 +0100

    feat: add Event.id, lat, lng

--- lib/event.dart
+++ lib/event.dart
@@ -5,13 +5,24 @@ import 'package:latlong2/latlong.dart';
 
 /// An emergency event that the ambulance service has become aware of.
 class Event {
-  Event({required this.address}) : location = _locationFromAddress(address);
+  Event({required this.id, required this.address})
+    : location = _locationFromAddress(address) {
+    debugPrint('Lat/lng for event $id: $lat, $lng');
+  }
+
+  final int id;
 
   /// The street address of the emergency.
   final String address;
 
+  /// Latitude.
+  double get lat => location.latitude;
+
   /// The latitude and longitude of the emergency.
   final LatLng location;
+
+  /// Longitude.
+  double get lng => location.longitude;
 
   /// Returns the latitude and longitude of a given street [address].
   static LatLng _locationFromAddress(String address) {
