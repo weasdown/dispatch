c67da2852044544947d12fe8f840c955b5abe16c
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-26 22:16:02 +0100

    feat: set centre of SCAS area as initial centre of map

--- lib/map.dart
+++ lib/map.dart
@@ -3,7 +3,16 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 /// A page that displays a Google Maps map.
 class MapPage extends StatefulWidget {
-  const MapPage({super.key});
+  const MapPage({super.key, this.centre = MapPage.scasCentre});
+
+  // Approximate geometric centre of the SCAS area.
+  static const LatLng scasCentre = LatLng(
+    51.453100204133726,
+    -1.307710460160255,
+  );
+
+  /// The point in the centre of the map when it is first opened.
+  final LatLng centre;
 
   @override
   State<MapPage> createState() => _MapPageState();
@@ -11,8 +20,6 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -24,7 +31,7 @@ class _MapPageState extends State<MapPage> {
       appBar: AppBar(title: const Text('Maps Sample App'), elevation: 2),
       body: GoogleMap(
         onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
+        initialCameraPosition: CameraPosition(target: widget.centre, zoom: 9),
       ),
     );
   }
