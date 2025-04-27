3e751204211ed3e3450b6dc44a5c616d88ee5e2d
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-27 17:54:59 +0100

    feat: add nocs list

--- lib/noc.dart
+++ lib/noc.dart
@@ -8,3 +8,9 @@ class NOC {
 
   final String description;
 }
+
+/// Full list of all the available Nature of Call statuses.
+List<NOC> nocs = List.unmodifiable([
+  NOC(Category.one, 'C1 Arrest/Peri Arrest C1'),
+  // TODO add all NOCs
+]);
