b3999bd070e1ebdab76a6e5ad6212e873196db83
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-27 19:26:42 +0100

    feat: add all Cat 1 NOCs

--- lib/noc.dart
+++ lib/noc.dart
@@ -19,6 +19,18 @@ class NOC {
 
 /// Full list of all the available Nature of Call statuses.
 List<NOC> nocs = List.unmodifiable([
-  NOC(Category.one, 'C1 Arrest/Peri Arrest C1'),
+  NOC(Category.one, 'CAT 1 - C1 Arrest/Peri Arrest C1'),
+  NOC(Category.one, 'CAT 1 - Choking C1'),
+  NOC(Category.one, 'CAT 1 - Drowning/Water Incident C1'),
+  NOC(Category.one, 'CAT 1 - Hanging C1'),
+  NOC(Category.one, 'CAT 1 - Fitting now C1'),
+  NOC(Category.one, 'CAT 1 - Level 1 HCP C1'),
+  NOC(Category.one, 'CAT 1 - Level 1 IFT C1'),
+  NOC(Category.one, 'CAT 1 - Life Threatening Asthma C1'),
+  NOC(Category.one, 'CAT 1 - Obstetric Emergency C1'),
+  NOC(Category.one, 'CAT 1 - Potential Anaphylaxis C1'),
+  NOC(Category.one, 'CAT 1 - Severe Bleeding under 5yrs C1'),
+  NOC(Category.one, 'CAT 1 - Unconscious pregnant >20 weeks C1'),
+  NOC(Category.one, 'CAT 1 - Unconscious under 16yrs C1'),
   // TODO add all NOCs
 ]);
