af1a6ef794d25bbd832f88df1316548355628e2d
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-28 20:20:59 +0100

    feat: add presentation folder with event_table.dart

---
+++ lib/presentation/event_table.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../logic/event.dart';
+
+/// A table displaying a list of [Event]s.
+class EventTable extends StatelessWidget {
+  const EventTable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
