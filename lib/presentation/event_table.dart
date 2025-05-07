7d76ac50d694df6c5b22f560d79f31395017b659
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-07 19:57:27 +0100

    feat: add basic EventTable

--- lib/presentation/event_table.dart
+++ lib/presentation/event_table.dart
@@ -4,10 +4,29 @@ import '../logic/event.dart';
 
 /// A table displaying a list of [Event]s.
 class EventTable extends StatelessWidget {
-  const EventTable({super.key});
+  const EventTable({super.key, required this.events});
+
+  final List<Event> events;
+
+  /// Returns a [TableRow] to display a given [event].
+  TableRow eventRow(Event event) => TableRow(children: [Text('Wow!')]);
+
+  static List<String> get headings => ['ID', 'Time', 'P'];
+
+  static TableRow get headingsRow => TableRow(
+    children: List<Widget>.from(
+      headings.map((String heading) => Text(heading)),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    print('In EventTable, events: $events');
+    return Table(
+      children: <TableRow>[
+        headingsRow,
+        // ...List<TableRow>.from(events.map((Event event) => eventRow(event))),
+      ],
+    );
   }
 }
