82b44a5c416565ec48e8c306ea86de316dab3337
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-07 23:48:06 +0100

    fix: add missing auth repository provider

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -1,6 +1,8 @@
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
+import '../data/repositories/auth/auth_repository.dart';
+import '../data/repositories/auth/auth_repository_dev.dart';
 import '../data/repositories/event/event_repository.dart';
 import '../data/repositories/event/event_repository_local.dart';
 import '../data/services/local_data_service.dart';
@@ -10,6 +12,7 @@ import '../data/services/local_data_service.dart';
 /// The user is always logged in.
 List<SingleChildWidget> get providersLocal {
   return [
+    ChangeNotifierProvider.value(value: AuthRepositoryDev() as AuthRepository),
     Provider.value(value: LocalDataService()),
     Provider(
       create:
