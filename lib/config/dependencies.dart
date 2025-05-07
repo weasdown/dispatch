b6b551268b51fd8e441b34f1c624f87cf7d2fff0
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-07 20:56:34 +0100

    feat: add providersLocal with instances of Provider

---
+++ lib/config/dependencies.dart
@@ -0,0 +1,21 @@
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+import '../data/repositories/event/event_repository.dart';
+import '../data/repositories/event/event_repository_local.dart';
+import '../data/services/local_data_service.dart';
+
+/// Configure dependencies for local data.
+/// This dependency list uses repositories that provide local data.
+/// The user is always logged in.
+List<SingleChildWidget> get providersLocal {
+  return [
+    Provider.value(value: LocalDataService()),
+    Provider(
+      create:
+          (context) =>
+              EventRepositoryLocal(localDataService: context.read())
+                  as EventRepository,
+    ),
+  ];
+}
