6cfa7b2243a142df530ce8077aee719103451c5a
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-08 12:23:27 +0100

    feat: add Person attributes

--- lib/domain/models/person/person.dart
+++ lib/domain/models/person/person.dart
@@ -1,1 +1,9 @@
-sealed class Person {}
+abstract class Person {
+  Person({required this.name, required this.address});
+
+  /// Where the person is **currently** located.
+  final String address;
+
+  /// The person's name.
+  final String name;
+}
