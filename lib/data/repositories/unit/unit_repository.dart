9522c03b7941a9108e6654a482cf3816c2dd97db
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-08 01:40:25 +0100

    feat: add unit_repository.dart

---
+++ lib/data/repositories/unit/unit_repository.dart
@@ -0,0 +1,10 @@
+import '../../../domain/models/unit.dart';
+import '../../../utils/result.dart';
+
+/// Data source for units.
+abstract class UnitRepository {
+  Future<Result<List<Unit>>> get allUnits;
+
+  /// Get a unit by its callsign.
+  Future<Result<Unit>> unitByCallsign(String callsign);
+}
