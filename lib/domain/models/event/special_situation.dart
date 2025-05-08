95944c708d73b32dfc87a3ef8f3a5994ce5e8b79
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-08 11:17:58 +0100

    feat: add special_situation.dart

---
+++ lib/domain/models/event/special_situation.dart
@@ -0,0 +1,8 @@
+import 'event.dart';
+
+/// Extra information about an [Event] that identifies something significant about the address or patient.
+class SpecialSituation {
+  SpecialSituation(this.description);
+
+  String description;
+}
