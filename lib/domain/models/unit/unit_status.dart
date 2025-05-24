a5da212875acefdf7fb4d05145237db0215e27ba
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-24 10:33:01 +0100

    feat: add remaining UnitStatus options

--- lib/domain/models/unit/unit_status.dart
+++ lib/domain/models/unit/unit_status.dart
@@ -3,8 +3,17 @@ library;
 
 /// Possible statuses for a [Unit] to show its current activity.
 enum UnitStatus {
-  ra('RA', 'Returning Available');
-  // TODO add other status options
+  ab('AB', 'Available Base'),
+  ra('RA', 'Returning Available'),
+  dp('DP', 'Dispatched'),
+  mo('MO', 'Mobile'),
+  as('AS', 'At Scene'),
+  wp('WP', 'With Patient'),
+  ls('LS', 'Left Scene'),
+  ah('AH', 'At Hospital'),
+  ho('HO', 'Handover'),
+  vc('VC', 'Vehicle Clear'),
+  os('OS', 'Out of Service');
 
   const UnitStatus(this.abbreviation, this.name);
 
