c06b5613975848a0c1b8dfc0cb2d6b7a12a889f1
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-26 01:01:47 +0100

    feat: add CFR option in VehicleType enum

--- app/lib/domain/models/unit/unit.dart
+++ app/lib/domain/models/unit/unit.dart
@@ -31,7 +31,8 @@ enum VehicleType {
   dca('Double-Crewed Ambulance'),
   rrv('Rapid Response Vehicle'),
   helicopter('Air Ambulance Helicopter'),
-  criticalCareCar('Critical Care Car');
+  criticalCareCar('Critical Care Car'),
+  communityFirstResponder('Community First Responder');
 
   const VehicleType(this.name);
 
