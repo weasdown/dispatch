fc9c51125f6cde0e654640a9158362f184ae835c
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-26 11:59:59 +0100

    feat: add _sharedProviders, providersRemote

--- app/lib/config/dependencies.dart
+++ app/lib/config/dependencies.dart
@@ -5,7 +5,11 @@ import '../data/repositories/auth/auth_repository.dart';
 import '../data/repositories/auth/auth_repository_dev.dart';
 import '../data/repositories/event/event_repository.dart';
 import '../data/repositories/event/event_repository_local.dart';
+import '../data/services/api/api_client.dart';
 import '../data/services/local_data_service.dart';
+
+/// Shared providers for all configurations.
+List<SingleChildWidget> _sharedProviders = [];
 
 /// Configure dependencies for local data.
 /// This dependency list uses repositories that provide local data.
@@ -15,10 +19,15 @@ List<SingleChildWidget> get providersLocal {
     ChangeNotifierProvider.value(value: AuthRepositoryDev() as AuthRepository),
     Provider.value(value: LocalDataService()),
     Provider(
-      create:
-          (context) =>
-              EventRepositoryLocal(localDataService: context.read())
-                  as EventRepository,
+      create: (context) =>
+          EventRepositoryLocal(localDataService: context.read())
+              as EventRepository,
     ),
   ];
+}
+
+/// Configure dependencies for remote data.
+/// This dependency list uses repositories that connect to a remote server.
+List<SingleChildWidget> get providersRemote {
+  return [Provider(create: (context) => ApiClient()), ..._sharedProviders];
 }
