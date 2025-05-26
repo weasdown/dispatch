aefb0d975f5732d16cb7615e38add667260cc8a8
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-26 11:39:38 +0100

    refactor: use LocalRepository mixin in EventRepositoryLocal

--- app/lib/data/repositories/event/event_repository_local.dart
+++ app/lib/data/repositories/event/event_repository_local.dart
@@ -2,34 +2,20 @@ import '../../../domain/models/event/event.dart';
 import '../../../domain/models/status.dart';
 import '../../../utils/result.dart';
 import '../../services/local_data_service.dart';
+import '../repository.dart';
 import 'event_repository.dart';
 
 /// Local implementation of EventRepository
-class EventRepositoryLocal implements EventRepository {
+class EventRepositoryLocal extends EventRepository with LocalRepository {
   EventRepositoryLocal({required LocalDataService localDataService})
     : _localDataService = localDataService;
 
-  // Only create default event once.
-  bool _isInitialized = false;
-  // Used to generate IDs for events.
-  int _sequentialId = 0;
-
-  final _events = List<Event>.empty(growable: true);
-  final LocalDataService _localDataService;
+  @override
+  Future<Result<List<Event>>> get allEvents => eventsList;
 
   // TODO implement allEvents getter (may need to be a List<Event> from a provider rather than a Stream)
   // /// Get a continuous stream of all the events.
   // Stream<Event> get allEvents;
-
-  Future<Result<List<Event>>> get eventsList async {
-    // Initialize the repository with a default event.
-    if (!_isInitialized) {
-      _events.addAll(_localDataService.events);
-      _isInitialized = true;
-    }
-
-    return Result.ok(_events);
-  }
 
   Future<void> createEvent({required String address, required NOC noc}) async {
     _events.add(Event.withNOC(id: _sequentialId++, address: address, noc: noc));
@@ -44,6 +30,26 @@ class EventRepositoryLocal implements EventRepository {
     return Result.ok(event);
   }
 
+  final _events = List<Event>.empty(growable: true);
+
+  Future<Result<List<Event>>> get eventsList async {
+    // Initialize the repository with a default event.
+    if (!_isInitialized) {
+      _events.addAll(localDataService.events);
+      _isInitialized = true;
+    }
+
+    return Result.ok(_events);
+  }
+
+  // Only create default event once.
+  bool _isInitialized = false;
+
+  final LocalDataService _localDataService;
+
   @override
-  Future<Result<List<Event>>> get allEvents => eventsList;
+  LocalDataService get localDataService => _localDataService;
+
+  // Used to generate IDs for events.
+  int _sequentialId = 0;
 }
