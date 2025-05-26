91280e126d8f56867e7c21e4dd34c61d829b36a1
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-26 11:38:37 +0100

    refactor: make EventRepository a subtype of Repository

--- app/lib/data/repositories/event/event_repository.dart
+++ app/lib/data/repositories/event/event_repository.dart
@@ -1,8 +1,9 @@
 import '../../../domain/models/event/event.dart';
 import '../../../utils/result.dart';
+import '../repository.dart';
 
 /// Data source for events.
-abstract class EventRepository {
+abstract class EventRepository extends Repository {
   Future<Result<List<Event>>> get allEvents;
 
   /// Get an event by its ID.
