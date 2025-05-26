8a61cb5de7b2b2f4c20a33ed8bc9196adc15f3a0
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-26 18:05:17 +0100

    feat: add Units.dispatchTo() method, assignedEvent and status getters

--- app/lib/domain/models/unit/unit.dart
+++ app/lib/domain/models/unit/unit.dart
@@ -1,18 +1,35 @@
 import 'package:latlng/latlng.dart';
 
+import '../event/event.dart';
+import 'unit_status.dart';
+
 /// An ambulance vehicle, air ambulance or other resource.
 class Unit {
   Unit({
     required this.callsign,
     required this.location,
     this.vehicleType = VehicleType.dca,
-  });
+    UnitStatus? status,
+  }) : _status = status ?? UnitStatus.pd;
 
   /// A unique identifier for this resource.
   final String callsign;
 
+  void dispatchTo(Event event) {
+    _assignedEvent = event;
+    _status = UnitStatus.dp;
+  }
+
+  Event? _assignedEvent;
+
+  Event? get assignedEvent => _assignedEvent;
+
   /// The current latitude and longitude of this unit.
   LatLng location;
+
+  UnitStatus _status;
+
+  UnitStatus get status => _status;
 
   Map<String, dynamic> toJson() => {
     'callsign': callsign,
