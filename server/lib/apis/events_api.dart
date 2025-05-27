47cbe1800ec484d8caf772e8e5f38c3a61c44e84
weasdown <34220924+weasdown@users.noreply.github.com>
2025-05-27 11:40:32 +0100

    feat: add placeholder api.dart, events_api.dart, units_api.dart

---
+++ server/lib/apis/events_api.dart
@@ -0,0 +1,10 @@
+import 'package:shelf_router/shelf_router.dart';
+
+import 'api.dart';
+
+/// Implements a simple events API.
+final class EventsApi extends Api {
+  @override
+  // TODO: implement router
+  Router get router => throw UnimplementedError();
+}
