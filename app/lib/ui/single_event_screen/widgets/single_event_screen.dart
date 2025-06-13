120c8aba8251e8cc6f1225cfd2ded2415d48213a
weasdown <34220924+weasdown@users.noreply.github.com>
2025-06-13 18:08:42 +0100

    feat: add logging to SingleEventScreen

--- app/lib/ui/single_event_screen/widgets/single_event_screen.dart
+++ app/lib/ui/single_event_screen/widgets/single_event_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 
 import '../../../domain/models/event/event.dart';
 import '../view_models/single_event_screen_viewmodel.dart';
@@ -13,6 +14,8 @@ class SingleEventScreen extends StatefulWidget {
 }
 
 class _SingleEventScreenState extends State<SingleEventScreen> {
+  final Logger _log = Logger('SingleEventScreen');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,19 +25,23 @@ class _SingleEventScreenState extends State<SingleEventScreen> {
         builder: (context, child) {
           Event? event = widget.viewModel.event;
 
-          return (event != null)
-              ? Table(
-                  children: [
-                    TableRow(children: [Text('ID'), Text(event.id.toString())]),
-                  ],
-                )
-              : Center(
-                  child: Text(
-                    'No event selected',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                );
+          if (event != null) {
+            _log.fine('There is a selected event');
+            return Table(
+              children: [
+                TableRow(children: [Text('ID'), Text(event.id.toString())]),
+              ],
+            );
+          } else {
+            _log.warning('There is no selected event');
+            return Center(
+              child: Text(
+                'No event selected',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            );
+          }
         },
       ),
     );
