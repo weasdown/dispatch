606844ea63152ce5b82498ed895ba9d4264e5a33
weasdown <34220924+weasdown@users.noreply.github.com>
2025-06-13 17:42:01 +0100

    feat: add initial layout to SingleEventScreen

--- app/lib/ui/single_event_screen/widgets/single_event_screen.dart
+++ app/lib/ui/single_event_screen/widgets/single_event_screen.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
 
+import '../../../domain/models/event/event.dart';
 import '../view_models/single_event_screen_viewmodel.dart';
 
-class SingleEventScreen extends StatelessWidget {
+class SingleEventScreen extends StatefulWidget {
   const SingleEventScreen({super.key, required this.viewModel});
 
   final SingleEventScreenViewModel viewModel;
 
   @override
+  State<SingleEventScreen> createState() => _SingleEventScreenState();
+}
+
+class _SingleEventScreenState extends State<SingleEventScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Event')),
+      body: ListenableBuilder(
+        listenable: widget.viewModel.load,
+        builder: (context, child) {
+          Event? event = widget.viewModel.event;
+
+          return (event != null)
+              ? Table(
+                  children: [
+                    TableRow(children: [Text('ID'), Text(event.id.toString())]),
+                  ],
+                )
+              : Center(
+                  child: Text(
+                    'No event selected',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                );
+        },
+      ),
+    );
   }
 }
