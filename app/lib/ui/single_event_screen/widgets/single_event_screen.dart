f85814b476c0b50f6113819857cdeef4ad5e9918
weasdown <34220924+weasdown@users.noreply.github.com>
2025-06-13 17:29:39 +0100

    feat: add single_event_screen.dart with placeholder SingleEventScreen widget

---
+++ app/lib/ui/single_event_screen/widgets/single_event_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../view_models/single_event_screen_viewmodel.dart';
+
+class SingleEventScreen extends StatelessWidget {
+  const SingleEventScreen({super.key, required this.viewModel});
+
+  final SingleEventScreenViewModel viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
