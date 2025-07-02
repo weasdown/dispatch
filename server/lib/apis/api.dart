e58187502bf6f9f1f1e3211b4cb02c466fc6cdc6
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-02 14:43:29 +0100

    feat: add Api.endpoints getter, router getter implementation

--- server/lib/apis/api.dart
+++ server/lib/apis/api.dart
@@ -1,5 +1,19 @@
 import 'package:shelf_router/shelf_router.dart';
 
+import 'endpoint.dart';
+
+/// A generic API.
 abstract class Api {
-  Router get router;
+  /// Definitions of this API's endpoints.
+  List<Endpoint> get endpoints;
+
+  Router get router {
+    final router = Router();
+
+    for (Endpoint endpoint in endpoints) {
+      endpoint.addToRouter(router: router);
+    }
+
+    return router;
+  }
 }
