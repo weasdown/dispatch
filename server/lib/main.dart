4b29e16d89ca1c1e8784f1eb031e10b21833e8cc
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-02 21:03:41 +0100

    feat: temporarily switch to HttpDispatchServer

--- server/lib/main.dart
+++ server/lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:dispatch/data/services/local_data_service.dart';
 import 'package:server/server.dart';
 
 void main() {
-  WebSocketServer.run();
+  // WebSocketServer.run();
+
+  // TODO switch back to WebSocketServer
+  HttpDispatchServer.run(localDataService: LocalDataService());
 }
