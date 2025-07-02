23bc9ab36b3c147e86d8dcc07af64f688b69399d
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-02 14:44:32 +0100

    fix: add missing overrides of Api.endpoints

--- server/lib/apis/events_api.dart
+++ server/lib/apis/events_api.dart
@@ -1,4 +1,10 @@
+import 'package:server/apis/endpoint.dart';
+
 import 'api.dart';
 
 /// Implements a simple events API.
-final class EventsApi extends Api {}
+final class EventsApi extends Api {
+  @override
+  // TODO: implement endpoints
+  List<Endpoint> get endpoints => throw UnimplementedError();
+}
--- server/lib/apis/units_api.dart
+++ server/lib/apis/units_api.dart
@@ -1,4 +1,10 @@
+import 'package:server/apis/endpoint.dart';
+
 import 'api.dart';
 
 /// Implements a simple units API.
-final class UnitsApi extends Api {}
+final class UnitsApi extends Api {
+  @override
+  // TODO: implement endpoints
+  List<Endpoint> get endpoints => throw UnimplementedError();
+}
