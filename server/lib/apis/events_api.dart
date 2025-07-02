9286769aa81509247768614219ec246226b9f53d
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-02 15:43:25 +0100

    feat: add EventsApi._getEventById Endpoint getter

--- server/lib/apis/events_api.dart
+++ server/lib/apis/events_api.dart
@@ -10,7 +10,27 @@ import 'api.dart';
 /// Implements a simple events API.
 final class EventsApi extends Api {
   @override
-  List<Endpoint> get endpoints => [_rootEndpoint];
+  List<Endpoint> get endpoints => [_rootEndpoint, _getEventById];
+
+  /// Gets an event by its id.
+  Endpoint get _getEventById => Endpoint(
+    method: HTTPMethod.get,
+    route: '/<id>',
+    handler: (Request request, String id) {
+      final Event? event = _events
+          .where((Event event) => event.id == id)
+          .firstOrNull;
+
+      if (event == null) {
+        return Response.notFound('No event found with id $id');
+      }
+
+      return Response.ok(
+        json.encode(event),
+        headers: {'Content-Type': 'application/json'},
+      );
+    },
+  );
 
   final List<Event> _events = Defaults.events;
 
