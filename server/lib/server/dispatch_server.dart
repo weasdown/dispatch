89e12a952833034559a8746691bedb7d503913ab
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-02 17:49:03 +0100

    feat: add dispatch_server.dart with abstract DispatchServer class

---
+++ server/lib/server/dispatch_server.dart
@@ -0,0 +1,55 @@
+import 'dart:io' show HttpServer;
+
+import 'package:dispatch/data/services/local_data_service.dart';
+import 'package:dispatch/domain/models/event/event.dart';
+import 'package:dispatch/domain/models/unit/unit.dart';
+import 'package:logging/logging.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
+abstract class DispatchServer {
+  DispatchServer({
+    String? scheme,
+    String? host,
+    int? port,
+    required Handler handler,
+  }) : _handler = handler,
+       _scheme = scheme ?? 'http',
+       _host = host ?? defaultHost,
+       _port = port ?? defaultPort,
+       _localDataService = LocalDataService();
+
+  static const String defaultHost = 'localhost';
+
+  static const int defaultPort = 8080;
+
+  final Handler _handler;
+
+  List<Event> get events => _localDataService.events;
+
+  final String _host;
+
+  final LocalDataService _localDataService;
+
+  final Logger _log = Logger('DispatchServer');
+
+  final int _port;
+
+  final String _scheme;
+
+  String get scheme => _scheme;
+
+  // FIXME refactor so server is higher-level than dart:io's HttpServer. Currently crashes when run on web because HttpServer isn't supported on web.
+  Future<HttpServer> serve() async =>
+      await shelf_io.serve(_handler, _host, _port).then((HttpServer server) {
+        final String logMessage =
+            'Serving at ws://${server.address.host}:${server.port}\n'
+            '\t- Current units: $units\n'
+            '\t- Current events: $events\n';
+        _log.info(logMessage);
+        print(logMessage);
+        return server;
+      });
+
+  List<Unit> get units => _localDataService.units;
+}
