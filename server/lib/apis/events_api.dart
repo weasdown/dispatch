d59f33eeddbcd93caadcab6f55f5dfb82b600148
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-02 14:44:04 +0100

    fix: remove redundant overrides of Api.router

--- server/lib/apis/events_api.dart
+++ server/lib/apis/events_api.dart
@@ -1,10 +1,4 @@
-import 'package:shelf_router/shelf_router.dart';
-
 import 'api.dart';
 
 /// Implements a simple events API.
-final class EventsApi extends Api {
-  @override
-  // TODO: implement router
-  Router get router => throw UnimplementedError();
-}
+final class EventsApi extends Api {}
--- server/lib/apis/units_api.dart
+++ server/lib/apis/units_api.dart
@@ -1,10 +1,4 @@
-import 'package:shelf_router/shelf_router.dart';
-
 import 'api.dart';
 
 /// Implements a simple units API.
-final class UnitsApi extends Api {
-  @override
-  // TODO: implement router
-  Router get router => throw UnimplementedError();
-}
+final class UnitsApi extends Api {}
