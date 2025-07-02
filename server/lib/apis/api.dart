6e492ae845a1b41be7475df9a607754549c0b7a1
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-02 16:22:17 +0100

    refactor: remove HttpApi logic from Api class

--- server/lib/apis/api.dart
+++ server/lib/apis/api.dart
@@ -1,19 +1,2 @@
-import 'package:shelf_router/shelf_router.dart';
-
-import 'endpoint.dart';
-
 /// A generic API.
-abstract class Api {
-  /// Definitions of this API's endpoints.
-  List<Endpoint> get endpoints;
-
-  Router get router {
-    final router = Router();
-
-    for (Endpoint endpoint in endpoints) {
-      endpoint.addToRouter(router: router);
-    }
-
-    return router;
-  }
-}
+abstract class Api {}
