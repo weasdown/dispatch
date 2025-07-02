5eb5e211ab6c0f499ed391cd3c152e3d57fd6c06
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-02 20:59:33 +0100

    feat: add Endpoint.parentRoute attribute

--- server/lib/apis/endpoint.dart
+++ server/lib/apis/endpoint.dart
@@ -3,10 +3,11 @@ import 'package:shelf_router/shelf_router.dart';
 /// A generic API endpoint
 class Endpoint {
   const Endpoint({
+    String? parentRoute,
     required this.method,
     required this.route,
     required this.handler,
-  });
+  }) : parentRoute = parentRoute ?? '';
 
   void addToRouter({required Router router}) =>
       router.add(method.verb, route, handler);
@@ -14,6 +15,8 @@ class Endpoint {
   final Function handler;
 
   final HTTPMethod method;
+
+  final String parentRoute;
 
   final String route;
 }
