64b68b084c8f5318cc437a460ca09eb834065ab7
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-02 14:50:14 +0100

    feat: add EventsApi._events attribute

--- server/lib/apis/events_api.dart
+++ server/lib/apis/events_api.dart
@@ -1,9 +1,12 @@
+import 'package:dispatch/domain/models/event/event.dart';
 import 'package:server/apis/endpoint.dart';
 
 import 'api.dart';
 
 /// Implements a simple events API.
 final class EventsApi extends Api {
+  final List<Event> _events = [];
+
   @override
   // TODO: implement endpoints
   List<Endpoint> get endpoints => throw UnimplementedError();
