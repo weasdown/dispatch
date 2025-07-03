76bfb7604a81a5a666a4e07a2082163441c58fd0
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-07-03 17:51:14 +0100

    feat: add HttpApi.respondOkJSON() method

--- server/lib/apis/http_api.dart
+++ server/lib/apis/http_api.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 import 'api.dart';
@@ -7,6 +10,13 @@ import 'endpoint.dart';
 mixin HttpApi implements Api {
   /// Definitions of this API's endpoints.
   List<Endpoint> get endpoints;
+
+  /// Encodes some [data] (e.g. a [Unit] or `List<Unit>`) as a JSON, then
+  /// delivers it as an HTTP OK [Response].
+  Response respondOkJSON(Object data) => Response.ok(
+    json.encode(data),
+    headers: {'Content-Type': 'application/json'},
+  );
 
   Router get router {
     final router = Router();
