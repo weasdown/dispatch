26dad0fbf5a1f6e7ef9194b82bf5c2dff8b3518d
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-07-03 14:54:44 +0100

    feat: rename main.dart to http_main.dart, make run HTTP server only

--- server/lib/http_main.dart
+++ server/lib/http_main.dart
@@ -2,8 +2,5 @@ import 'package:dispatch/data/services/local_data_service.dart';
 import 'package:server/server.dart';
 
 void main() {
-  // WebSocketServer.run();
-
-  // TODO switch back to WebSocketServer
   HttpDispatchServer.run(localDataService: LocalDataService());
 }
